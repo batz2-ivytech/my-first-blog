3e5834ff2b96a38442cf761630f464bd23624504
Brice Atz <batz2@ivytech.edu>
2026-04-30 20:29:02 -0400

    Added comment model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+class Comment(models.Model):
+    # Link to Post; if a Post is deleted, its comments are too (CASCADE)
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
